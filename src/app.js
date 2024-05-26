7e4cd8d5d0c799e8ba707aaa53bb06d518f8480b
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-26 14:21:11 +0530

    improve: improve code quality structure

---
+++ src/app.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+//serve the static files
+app.use(express.static("public"));
+
+// cross origin : block the unwanted request
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URI,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//
+app.get("/", (req, res) => {
+  res.send("Don't panic here i am running correctly ");
+});
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import express from "express";
+/* eslint-disable no-constant-binary-expression */
+/* eslint-disable no-undef */
+import dotenv from "dotenv"; // add this in scripts for import : "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+import { app } from "./app.js";
 
-const app = express();
-const port = 3000 || "env variable ";
-
-app.get("/", (req, res) => {
-  res.send("Chalo aaj kuch karte hai ");
+// env configuritaion
+dotenv.config({
+  path: './env'
 });
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+const port = 4000 ||  process.env.PORT 
+
+
+try {
+    app.on("error",(err)=>{
+        console.log('error while starting server ::',err)
+    })
+
+
+    //app listener start 
+    app.listen(port,()=>{
+        console.log(`server is running on port :: http://localhost:${port}`)
+    })
+} catch (error) {
+    console.log('error while running app ',error)
+}
